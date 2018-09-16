9f589b6c31e44a2b81efe4bb8d838a461ca09327
Joaquin Sanchez <sanchezjoaquin1995@gmail.com>
2018-09-16 00:40:28 -0300

    Calculating model accuracy

--- random_choose.py
+++ random_choose.py
@@ -8,9 +8,18 @@ FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 forumKeys = msgpack.unpack(open('./input/forumKeys.msgpack', 'rb'))
 
-for i in range(0, 5):
-	rand = randint(0, len(forumKeys)-1)
-	predicted = forumKeys[rand]
 
-	if predicted[2] == train_y[i].tolist():
-		print('Match:' + forumKeys[rand][1].decode('ascii') + '\n')
+with open('./output/random_accuracy.txt', 'w') as f:
+	for r in range(0, 100):
+		correct = 0
+		total_comparations = len(train_y)
+		for i in range(0, total_comparations):
+			rand = randint(0, len(forumKeys)-1)
+			predicted = forumKeys[rand]
+
+			if predicted[2] == train_y[i].tolist():
+				correct += 1
+
+		accuracy = correct/total_comparations
+		print('Random accuracy: ' + str(accuracy))
+		f.write(str(r+1) + '\t' + str(accuracy) + '\n')
