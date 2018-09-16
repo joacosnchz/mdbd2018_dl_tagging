07ca90ab9e024ae2dd339743e3aec5029e176749
Agustin <agus.delmonti@gmail.com>
2018-09-16 00:38:01 -0300

    Quitar timediff

--- trainings.bat
+++ trainings.bat
@@ -1,6 +1,9 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 SET BATCH_SIZE=0
 
+
+rem batch_size, epochs per training, using, categories, index
+
 FOR /L %%A IN (1,1,10) DO (
 	SET /A BATCH_SIZE=!BATCH_SIZE!+10
 	python .\tags_net.py !BATCH_SIZE! 100 1 15 %%A
